eaf58d92614f3135d808869f47d587a3c0933782
likai <1195419506@qq.com>
2022-02-14 23:43:28 +0800

    feat: 客户端传入未在dto中定义的参数时对其进行报错处理

---
+++ README.md
@@ -0,0 +1,2 @@
+# nest-project 
+使用nest构建的服务端应用，有关此项目的更多介绍请移步：[使用NestJS搭建服务端应用.md](https://juejin.cn/post/7053840108331466783)
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true })
+  );
   await app.listen(3000);
 }
 bootstrap();
