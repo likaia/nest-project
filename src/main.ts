1401cdba18c0f48a4396f2c9cab3cd7449f30c25
likai <1195419506@qq.com>
2022-02-16 23:39:04 +0800

    feat: dto参数校验不通过时自定义其错误信息

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./module/AppModule";
-import { ValidationPipe } from "@nestjs/common";
+import {
+  BadRequestException,
+  ValidationError,
+  ValidationPipe
+} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true })
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        console.log(errors);
+        const propertyNotExist = [];
+        for (let i = 0; i < errors.length; i++) {
+          const errorMessage = "属性" + errors[i].property + "未定义";
+          propertyNotExist.push(errorMessage);
+        }
+        return new BadRequestException(propertyNotExist);
+      }
+    })
   );
   await app.listen(3000);
 }
