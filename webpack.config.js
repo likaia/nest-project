af59332fb82ccdc443f47defcb4dbb11561a4461
likai <1195419506@qq.com>
2022-03-18 12:41:49 +0800

    build: 修复nest构建时swagger无法访问的问题

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
 const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 module.exports = {
@@ -55,6 +57,27 @@ module.exports = {
         return false;
       }
     }),
-    new ForkTsCheckerWebpackPlugin()
+    new ForkTsCheckerWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        // 拷贝swagger相关的文件
+        {
+          from: __dirname + "/node_modules/swagger-ui-dist/",
+          to: "./"
+        }
+      ]
+    }),
+    // 删除多余的文件
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: [
+        __dirname + "/dist/*.html",
+        __dirname + "/dist/*.map",
+        __dirname + "/dist/*.md",
+        __dirname + "/dist/*.json",
+        __dirname + "/dist/index.js",
+        __dirname + "/dist/LICENSE",
+        __dirname + "/dist/NOTICE"
+      ]
+    })
   ]
 };
